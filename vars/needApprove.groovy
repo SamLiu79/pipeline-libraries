25db7d87c04d8822e42b2174666752dc724c2d37
Sam Liu <fla.sam@aspiraconnect.com>
2019-11-15 10:52:45 +0800

    remove unused code

--- vars/needApprove.groovy
+++ vars/needApprove.groovy
@@ -10,17 +10,13 @@ def call(body) {
 
     def timeoutTime = config.timeoutTime ?: 24
     def version = config.version ?: "current"
-    def proceedMessage = """Would you like to promote version ${version} to the next environment?
+    def proceedMessage = """Would you like to promote ${version} version to the next environment?
 """
-    def id = approveRequestedEvent(app: "${env.JOB_NAME}", environment: config.environment)
-
     try {
         timeout(time: timeoutTime, unit: 'HOURS') {
             input id: 'Proceed', message: "\n${proceedMessage}"
         }
     } catch (err) {
-        approveReceivedEvent(id: id, approved: false)
         throw err
     }
-    approveReceivedEvent(id: id, approved: true)
 }
